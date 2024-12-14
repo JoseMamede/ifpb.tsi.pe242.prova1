967bb62ce0c92b9039137d276f3b6a94faf6e2f1
JoseMamede <mamede.jose@academico.ifpb.edu.br>
2024-12-14 12:37:43 +0000

    save2

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def q2():
     """
     Dado um numeral romano, converta-o para um número inteiro.
     """
-    #Dado um numeral romano, converta-o para um número inteiro
     user = input("")
     romanos = [1,5,10,50,100,500,1000]
     I,V,X,L,C,D,M = romanos
@@ -22,24 +21,29 @@ def q2():
     for i in user:
         b +=(romanos[i])
     print(b)
-    #Rasnho
+
 
 def q3():
     numb = int(input("Digite um número:"))
     contador = 1
 
-    while contador / numb:
-        print(f"Contador: {contador}")
-        contador += 1
-    #Rascunho
+
 
 def q4():
-    n1 = int(float(input("Digite um número:")))
-    n2 = int(float(input("Digite um número:")))
+    n1 = int(input("Digite um número:"))
+    n2 = int(input("Digite um número:"))
     soma = 0
-    for i in range(n1, n2):
-        print(i)
-        #Rascunho
+
+    if n1 < n2:
+        for i in range(n1, n2+1):
+            if i > 0:
+                soma += i
+    else:
+        for i in range(n2, n1+1):
+            if i > 0:
+                soma += i
+
+    print(soma)
 
     
 
